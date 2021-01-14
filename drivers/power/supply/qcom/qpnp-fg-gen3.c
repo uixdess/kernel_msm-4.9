cb76d6a44230dffc05c0a0f254d5590fe597776c
Albert I <kras@raphielgang.org>
2021-01-14 16:22:38 +0300

    drivers: power/qcom: fg-gen3: Don't set maximum constant charging current
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I07fc9fcb4c338bdb5fef504578e60ad01000da55
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1203,7 +1203,7 @@ static void fg_notify_charger(struct fg_chip *chip)
 			rc);
 		return;
 	}
-
+/*
 	prop.intval = chip->bp.fastchg_curr_ma * 1000;
 	rc = power_supply_set_property(chip->batt_psy,
 			POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX, &prop);
@@ -1212,7 +1212,7 @@ static void fg_notify_charger(struct fg_chip *chip)
 			rc);
 		return;
 	}
-
+*/
 	fg_dbg(chip, FG_STATUS, "Notified charger on float voltage and FCC\n");
 }
 
