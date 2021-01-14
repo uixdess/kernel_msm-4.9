e0e8e73888d9a814243e7337798b362918aa23a4
Albert I <kras@raphielgang.org>
2021-01-14 16:28:46 +0300

    drivers: power/qcom: smb2: Import Xiaomi changes for thermal mitigation logic
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: Ia4a9b4e0f989c59a1f4abd4ee850709c496492d0
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -326,7 +326,14 @@ struct smb_charger {
 	int			boost_threshold_ua;
 	int			system_temp_level;
 	int			thermal_levels;
+#ifdef CONFIG_THERMAL
+	int			*thermal_mitigation_dcp;
+	int			*thermal_mitigation_qc3;
+	int			*thermal_mitigation_qc2;
+	int			*thermal_mitigation_pd_base;
+#else
 	int			*thermal_mitigation;
+#endif
 	int			dcp_icl_ua;
 	int			fake_capacity;
 	int			fake_batt_status;
